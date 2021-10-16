b75398d7c604ad999e1e9f2e4b8d490bed6bf1e7
koutamanto <58100773+koutamanto@users.noreply.github.com>
2021-10-16 16:42:22 +0900

    Add send_messagesV3.py

--- test/get_messages.py
+++ test/get_messages.py
@@ -1,18 +1,19 @@
 import requests, json
 from login import login
 
-datas = login()
+def get_messages():    
+    datas = login()
 
-for group in datas["group"]:
-    gid = group["group_uid"]
-    print(gid)
-r = requests.post("http://163.44.249.252/get_message", json={"type":"get_message", "gid":input("gid:")})
-r_datas = json.loads(r.text)["datas"]
-print(r, r_datas)
-for message in r_datas:
-    content = message["content"]
-    name = message["name"]
-    uid = message["uid"]
-    number = message["number"]
-    unix = message["unix"]
-    print(content, name, uid, number, unix)
+    for group in datas["group"]:
+        gid = group["group_uid"]
+        print(gid)
+    r = requests.post("http://163.44.249.252/get_message", json={"type":"get_message", "gid":input("gid:")})
+    r_datas = json.loads(r.text)["datas"]
+    print(r, r_datas)
+    for message in r_datas:
+        content = message["content"]
+        name = message["name"]
+        uid = message["uid"]
+        number = message["number"]
+        unix = message["unix"]
+        print(content, name, uid, number, unix)
---
+++ test/send_messagev3.py
@@ -0,0 +1,18 @@
+import requests, json
+from login import login
+from get_list import get_list
+from get_messages import get_messages
+
+datas = login()
+uid = datas["uid"]
+gids = get_list()
+for gid in gids:
+    print(gid)
+to = input("to(gid):")
+
+while True:
+    text = input("text:")
+    datas = {"from":uid, "to":to, "message":{"type":"text", "content":text}}
+    r = requests.post("http://163.44.249.252/send_message", json=datas)
+    print(r, r.text)
+    get_messages()
